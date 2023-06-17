cbfc9ab803cb885d052ce419eac52b31af413dbf
zecelik <zecelik0@gmail.com>
2023-06-17 13:55:51 +0300

    Initial commit

---
+++ ft_printf.h
@@ -0,0 +1,17 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <stdio.h>
+# include <unistd.h>
+# include <stdbool.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+int	ft_printf(const char *str, ...);
+int	ft_format(va_list arg, char c);
+int	ft_int(int a);
+int	ft_hex(unsigned int a, char c);
+int	ft_point(unsigned long a, int sign);
+int	ft_string(char *str);
+int	ft_unsigned(unsigned int a);
+
+#endif
